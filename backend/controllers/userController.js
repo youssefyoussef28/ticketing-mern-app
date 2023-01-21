7a8a9f6bdce88d5a66bf066a7eb4be5045836da5
Youssef Youssef <youssefyoussef28@gmail.com>
2023-01-21 21:07:49 +0200

    Created the Error Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,18 @@
+// @route /api/users
+// Public
+
 const registerUser = (req, res) => {
-  res.send("Registering a User");
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+  res.json({ message: "Registering a User" });
 };
+
+// @route /api/users/login
+// Public
 const loginUser = (req, res) => {
   res.send("Login User");
 };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: `Welcome to the supportDesk API` });
 });
-
-app.use("/users", require("./routes/userRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server Started on port ${PORT}`);
