7b80e994b66cd8278831e6f6a760863526e566fc
Youssef Youssef <youssefyoussef28@gmail.com>
2023-01-22 23:20:20 +0200

    Registering User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+
 // @route /api/users
 // Public
 const registerUser = asyncHandler(async (req, res) => {
@@ -8,7 +11,33 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please include all fields");
   }
-  res.json({ message: "Registering a User" });
+  // Find if user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error(`User already exists`);
+  }
+  // Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user Data");
+  }
 });
 
 // @route /api/users/login
