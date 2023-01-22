ae641cd896e889ab45524080606694dc5aab480e
Youssef Youssef <youssefyoussef28@gmail.com>
2023-01-22 22:58:53 +0200

    Connected to MONGO DB

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const colors = require("colors");
 const PORT = process.env.PORT || 5000;
 
+connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
