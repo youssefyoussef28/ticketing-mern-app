3cfe2fbe1a25bf07f110121b04843b76a0645421
Youssef Youssef <youssefyoussef28@gmail.com>
2023-01-25 22:15:45 +0200

    Header and Initial pages setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Fragment } from "react";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </Fragment>
+  );
 }
 
 export default App;
