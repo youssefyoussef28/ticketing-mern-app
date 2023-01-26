af02a04c5783ca12fa1b7e10c631be24afec0a93
Youssef Youssef <youssefyoussef28@gmail.com>
2023-01-26 12:41:36 +0200

    Created the Login Form after completing the register form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,69 @@
 import React from "react";
+import { useState } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt />
+          Login
+        </h1>
+        <p>Please Log in to get support</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder={"Enter your email"}
+              required
+            />
+          </div>{" "}
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              id="password"
+              value={password}
+              onChange={onChange}
+              placeholder={"Enter your password"}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Login;
